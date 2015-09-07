391286bab29de4530f6f9f35aac23baeb4f3b28b
Cody Rapp <codydoc@gmail.com>
2015-09-06 22:58:20 -0700

    Assignment 11 completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,6 +15,31 @@
 
     /* WORK HERE */
     
+    if(dollars == 4){
+    
+        itemToReturn = @"get out of my store";
+    }
+    
+    if(dollars == 5){
+    
+        itemToReturn = @"have some gum";
+    }
+    
+    if(dollars == 6){
+    
+        itemToReturn = @"have an apple";
+    }
+    
+    if(dollars == 1000){
+    
+        itemToReturn = @"have an Apple computer";
+    }
+    
+    if(dollars == 1000000000){
+    
+        itemToReturn = @"have The Big Apple";
+    }
+    
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
@@ -24,9 +49,13 @@
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
+    /*
+     if (self.getsDiscount) {
         cost *= .75;
     }
+     */
+    
+    cost = self.getsDiscount ? cost*=.75 : cost;
     
     return cost;
 }
