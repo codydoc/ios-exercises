27ecb8341fcac4155a5531be03b5c59dfba90ec1
Cody Rapp <codydoc@gmail.com>
2015-09-08 18:41:57 -0700

    Assignment 12 completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,46 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    NSInteger lowNumber;
+    NSInteger highNumber;
+    
+    if(number == otherNumber){
+    
+        NSString *noGo = [NSString stringWithFormat:@"%li",(long)number];
+        return noGo;
+    }
+    
+    if(number<otherNumber){
+    
+        lowNumber = number;
+        highNumber = otherNumber;
+    }
+    
+    
+    else {
+        lowNumber = otherNumber;
+        highNumber = number;
+        
+    }
+    
+    NSString *string1 = [NSString stringWithFormat:@"%li",(long)lowNumber];
+    
+    //NSLog(string1);
+    
+    while(lowNumber<highNumber){
+    
+        lowNumber++;
+        
+        NSString *stringNow = [NSString stringWithFormat:@"%li",(long)lowNumber];
+        
+        string1 = [string1 stringByAppendingString:stringNow];
+        //NSLog(string1);
+    }
+    
+    
+    
+    return string1;
 }
 
 @end
