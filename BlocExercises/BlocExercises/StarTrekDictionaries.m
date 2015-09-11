04a34048ac5c07bbbd04cd7da5b4d61dada8c7be
Cody Rapp <codydoc@gmail.com>
2015-09-10 23:06:49 -0700

    Assignment 14 completed

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,37 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    
+    return characterDictionary[@"favorite drink"];
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *characterFoods = [[NSMutableArray alloc] init];
+    //Note I tried the above without the = [[NSMutableArray alloc] init] and it didn't work
+    
+    for (int i=0; i<[charactersArray count];i++){
+    
+        [characterFoods addObject:[[charactersArray objectAtIndex:i] objectForKey: @"favorite drink"] ];
+    
+    }
+    
+    //Could I have done this w/ enumerateObjectUsingBlock method?
+    
+    NSLog(@"%@",characterFoods);
+    
+    return characterFoods;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    
+    NSMutableDictionary *mutableCharacterDictionary = [characterDictionary mutableCopy];
+    
+    [mutableCharacterDictionary setObject:@"Here is a memorable quote" forKey: @"quote"];
+    
+    return mutableCharacterDictionary;
 }
 
 @end
